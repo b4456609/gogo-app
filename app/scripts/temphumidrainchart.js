2f51742d0199d0c316989c3f98a9398f300808cf
Bernie <b4456609@gmail.com>
2016-05-18 16:31:00 +0800

    seperate rain and bar chart

--- app/scripts/temphumidrainchart.js
+++ app/scripts/temphumidrainchart.js
@@ -1,14 +1,15 @@
 function drawTempHumid(data) {
 
-	var time = data.time.map(function (i) {
-		console.log(i,moment(i))
-		return moment(i).tz('Asia/Taipei').format('LT')
-	})
-  drawTempHumidChart(time, data.temp, data.humid, data.rain);
+  var time = data.time.map(function(i) {
+    console.log(i, moment(i))
+    return moment(i).tz('Asia/Taipei').format('LT')
+  })
+  drawTempHumidChart(time, data.temp, data.humid);
+  drawRainChart(time, data.rain);
 }
 
 function drawTempHumidChart(labels, temp, humid, rain) {
-	Chart.defaults.global.maintainAspectRatio = false;
+  Chart.defaults.global.maintainAspectRatio = false;
   console.log(labels, temp, humid)
   var ctx1 = document.getElementById('tempChart');
   var myChart1 = new Chart(ctx1, {
@@ -16,28 +17,7 @@ function drawTempHumidChart(labels, temp, humid, rain) {
     data: {
       labels: labels,
       datasets: [{
-        label: 'Temp',
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: temp,
-				yAxisID: 'tempy'
-      },{
-        label: 'Humid',
+        label: '溫度',
         fill: false,
         lineTension: 0.1,
         backgroundColor: 'rgba(255,99,132,0.4)',
@@ -55,23 +35,78 @@ function drawTempHumidChart(labels, temp, humid, rain) {
         pointHoverBorderWidth: 2,
         pointRadius: 1,
         pointHitRadius: 10,
+        data: temp,
+        yAxisID: 'tempy'
+      }, {
+        label: '濕度',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
         data: humid,
-				yAxisID: 'humidy'
+        yAxisID: 'humidy'
       }]
     },
     options: {
       scales: {
         yAxes: [{
-					id: 'tempy',
+          id: 'tempy',
           ticks: {
-						suggestedMax: 34,
-            suggestedMin: 14
+            suggestedMax: 34,
+            suggestedMin: 14,
+            fontColor:'rgba(255,99,132,1)',
+            callback:function (value) { return '' + value + '°C'}
           }
-        },{
-					id: 'humidy',
+        }, {
+          id: 'humidy',
           ticks: {
-						suggestedMax: 100,
-            suggestedMin: 50
+            suggestedMax: 100,
+            suggestedMin: 50,
+            fontColor:'rgba(75,192,192,1)',
+            callback:function (value) { return '' + value + '％'}
+          }
+        }]
+      }
+    }
+  });
+}
+
+function drawRainChart(time, rain) {
+  var ctx = document.getElementById('rainChart');
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: time,
+      datasets: [{
+        label: '降雨量',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: rain,
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            suggestedMax: 5,
+            suggestedMin: 0,
+            callback:function (value) { return '' + value + 'mm'}
           }
         }]
       }
