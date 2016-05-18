2e5b0118261c995e4c5c299c24a9991b187fb7f9
Bernie <b4456609@gmail.com>
2016-05-18 14:32:27 +0800

    mix temp humid

--- app/scripts/temphumidrainchart.js
+++ app/scripts/temphumidrainchart.js
@@ -8,6 +8,7 @@ function drawTempHumid(data) {
 }
 
 function drawTempHumidChart(labels, temp, humid, rain) {
+	Chart.defaults.global.maintainAspectRatio = false;
   console.log(labels, temp, humid)
   var ctx1 = document.getElementById('tempChart');
   var myChart1 = new Chart(ctx1, {
@@ -33,20 +34,44 @@ function drawTempHumidChart(labels, temp, humid, rain) {
         pointHoverBorderWidth: 2,
         pointRadius: 1,
         pointHitRadius: 10,
-        data: temp
-      }, {
-        label: 'humid',
-        data: humid
-      }, {
-        label: 'rain',
-        data: rain
+        data: temp,
+				yAxisID: 'tempy'
+      },{
+        label: 'Humid',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(255,99,132,0.4)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(255,99,132,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(255,99,132,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: humid,
+				yAxisID: 'humidy'
       }]
     },
     options: {
       scales: {
         yAxes: [{
+					id: 'tempy',
           ticks: {
-            beginAtZero: true
+						suggestedMax: 34,
+            suggestedMin: 14
+          }
+        },{
+					id: 'humidy',
+          ticks: {
+						suggestedMax: 100,
+            suggestedMin: 50
           }
         }]
       }
