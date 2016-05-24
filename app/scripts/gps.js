823b249f47497679ad7047017ae5a80337429b07
Bernie <b4456609@gmail.com>
2016-05-24 20:03:23 +0800

    add statics to gps

--- app/scripts/gps.js
+++ app/scripts/gps.js
@@ -15,6 +15,28 @@ $(function() {
     async: true
   }).on('loaded', function(e) {
     map.fitBounds(e.target.getBounds());
+    console.log(e.target.get_name());
+    console.log(e.target.get_distance());
+    console.log(e.target.get_start_time());
+    console.log(e.target.get_end_time());
+    console.log(e.target.get_moving_time());
+    console.log(e.target.get_moving_pace());
+    console.log(e.target.get_elevation_gain());
+    console.log(e.target.get_elevation_loss());
+    console.log(e.target.get_elevation_data())
+
+    var total_duration = moment.duration(e.target.get_total_time());
+    var moving_duration = moment.duration(e.target.get_moving_time());
+
+    $('#dis').text(Math.round(e.target.get_distance())+'m');
+    $('#start_time').text(moment(e.target.get_start_time()).tz('Asia/Taipei').format('LLLL'));
+    $('#end_time').text(moment(e.target.get_end_time()).tz('Asia/Taipei').format('LLLL'));
+    $('#total_time').text(total_duration.hours() + ':' + total_duration.minutes());
+    $('#moving_time').text(moving_duration.hours() + ':' + moving_duration.minutes());
+    $('#moving_speed').text(Math.round(e.target.get_moving_speed() * 100) / 100 +'km/h');
+    $('#up').text(Math.round(e.target.get_elevation_gain())+'m');
+    $('#down').text(Math.round(e.target.get_elevation_loss())+'m');
+
   }).on('addline', function(e) {
     el.addData(e.line);
   }).addTo(map);
